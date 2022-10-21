954d7cfe3717329fc876431e9a3fc5fedd08bfee
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-10-21 19:40:45 -0300

    feat: a users model

---
+++ app/backend/src/database/models/User.ts
@@ -0,0 +1,56 @@
+import { Model, INTEGER, STRING, DECIMAL } from 'sequelize';
+import db from '.';
+
+class Example extends Model {
+  id: number;
+  username: string;
+  role: string;
+  email: string;
+  password: string;
+}
+
+Example.init(
+  {
+    id: {
+      type: INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: STRING(30),
+      allowNull: false,
+    },
+    role: {
+      allowNull: false,
+      type: STRING,
+    },
+    email: {
+      type: STRING,
+      allowNull: false,
+    },
+    password: {
+      type: STRING,
+      allowNull: false,
+    },
+  },
+  {
+    underscored: true,
+    sequelize: db,
+    modelName: 'example',
+    timestamps: false,
+  },
+);
+
+/**
+  * `Workaround` para aplicar as associations em TS:
+  * Associations 1:N devem ficar em uma das instâncias de modelo
+  * */
+
+// OtherModel.belongsTo(Example, { foreignKey: 'campoA', as: 'campoEstrangeiroA' });
+// OtherModel.belongsTo(Example, { foreignKey: 'campoB', as: 'campoEstrangeiroB' });
+
+// Example.hasMany(OtherModel, { foreignKey: 'campoC', as: 'campoEstrangeiroC' });
+// Example.hasMany(OtherModel, { foreignKey: 'campoD', as: 'campoEstrangeiroD' });
+
+export default Example;
