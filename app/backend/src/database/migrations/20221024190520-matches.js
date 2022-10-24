9a3c3ed476a172ea6b3ff154531f643b1cc8b141
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-10-24 20:59:41 -0300

    feat: now there is a table for soccer matches, in DB

---
+++ app/backend/src/database/migrations/20221024190520-matches.js
@@ -0,0 +1,54 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    queryInterface.createTable("matches", {
+      id: {
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+      },
+      homeTeam: {
+        field: "home_team",
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
+        foreignKey: {
+          model: "Team",
+          key: "id",
+        },
+      },
+      homeTeamGoals: {
+        field: "home_team_goals",
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      awayTeam: {
+        field: "away_team",
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
+        foreignKey: {
+          model: "Team",
+          key: "id",
+        },
+      },
+      awayTeamGoals: {
+        field: "away_team_goals",
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      inProgress: {
+        field: "in_progress",
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+    });
+  },
+
+  down: async (queryInterface, _Sequelize) => {
+    queryInterface.dropTable("matches");
+  },
+};
