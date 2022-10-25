3e41bbab3ed004fc11d15005c3853773820ebe23
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-10-24 21:02:38 -0300

    feat: the login controller now has a middleware for validating it body request fields

---
+++ app/backend/src/middlewares/loginMiddlewares.ts
@@ -0,0 +1,23 @@
+import * as express from 'express';
+import * as bcrypt from 'bcryptjs';
+
+import UserServices from '../services/user.service';
+import loginValidation from '../services/validation/loginValidation';
+
+const verifyLoginFields: express.RequestHandler = async (req, res, next) => {
+  const { body } = req;
+  const { error } = loginValidation.validate(body);
+  if (error) {
+    return res.status(400).json({ message: 'All fields must be filled' });
+  }
+  const user = await new UserServices().findUserByEmail(body.email);
+  if (!user) return res.status(401).json({ message: 'Incorrect email or password' });
+  const userPass = user.password as string;
+  const passValidation = await bcrypt.compare(body.password, userPass);
+  if (user.email !== body.email || !passValidation) {
+    return res.status(401).json({ message: 'Incorrect email or password' });
+  }
+  next();
+};
+
+export default verifyLoginFields;
