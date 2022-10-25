6e17987e459557c8f39966d4782f3f8f0f9efd4d
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-10-25 15:07:59 -0300

    feat: a controller for login/validate route

--- app/backend/src/controllers/login.controller.ts
+++ app/backend/src/controllers/login.controller.ts
@@ -1,12 +1,20 @@
 import * as express from 'express';
+import LoginServices from '../services/login.service';
 import createJWT from '../utils/createJWT';
 
 class LoginController {
+  constructor(private loginServices = new LoginServices()) {}
+
   public login: express.RequestHandler = (req, res) => {
     const { body } = req;
-    console.log(body);
     const token = createJWT(body?.email);
     res.status(200).json({ token });
+  };
+
+  public loginValidate: express.RequestHandler = async (req, res) => {
+    const { headers } = req;
+    const role = await this.loginServices.getUserRole(<{ authorization:'' }>headers);
+    res.status(200).json({ role });
   };
 }
 
