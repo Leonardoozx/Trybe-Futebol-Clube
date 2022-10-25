5582b3536161c045c9fd6f0ded50d4c29c2dcc48
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-10-25 15:09:30 -0300

    feat: a middleware for verifying the header from login requests

--- app/backend/src/middlewares/loginMiddlewares.ts
+++ app/backend/src/middlewares/loginMiddlewares.ts
@@ -1,23 +1,37 @@
 import * as express from 'express';
 import * as bcrypt from 'bcryptjs';
+import * as jwt from 'jsonwebtoken';
 
 import UserServices from '../services/user.service';
 import loginValidation from '../services/validation/loginValidation';
 
-const verifyLoginFields: express.RequestHandler = async (req, res, next) => {
-  const { body } = req;
-  const { error } = loginValidation.validate(body);
-  if (error) {
-    return res.status(400).json({ message: 'All fields must be filled' });
-  }
-  const user = await new UserServices().findUserByEmail(body.email);
-  if (!user) return res.status(401).json({ message: 'Incorrect email or password' });
-  const userPass = user.password as string;
-  const passValidation = await bcrypt.compare(body.password, userPass);
-  if (user.email !== body.email || !passValidation) {
-    return res.status(401).json({ message: 'Incorrect email or password' });
-  }
-  next();
-};
+const { JWT_SECRET } = process.env;
 
-export default verifyLoginFields;
+class LoginMiddlewares {
+  public verifyLoginFields: express.RequestHandler = async (req, res, next) => {
+    const { body } = req;
+    const { error } = loginValidation.validate(body);
+    if (error) {
+      return res.status(400).json({ message: 'All fields must be filled' });
+    }
+    const user = await new UserServices().findUserByEmail(body.email);
+    if (!user) return res.status(401).json({ message: 'Incorrect email or password' });
+    const userPass = user.password as string;
+    const passValidation = await bcrypt.compare(body.password, userPass);
+    if (user.email !== body.email || !passValidation) {
+      return res.status(401).json({ message: 'Incorrect email or password' });
+    }
+    next();
+  };
+
+  public verifyLoginHeader: express.RequestHandler = (req, res, next) => {
+    const { headers } = req;
+    const token = headers.authorization;
+    if (!token) return res.status(400).json({ message: 'Unauthorized user' });
+    const tokenValidation = jwt.verify(token, JWT_SECRET as string);
+    if (!tokenValidation) return res.status(401).json({ message: 'invalid token' });
+    next();
+  };
+}
+
+export default LoginMiddlewares;
