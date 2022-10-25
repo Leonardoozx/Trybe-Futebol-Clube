ee7f74e8f064e5395266fbb089c39b01eac00299
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-10-24 21:00:30 -0300

    feat: implements the Team model

---
+++ app/backend/src/database/models/Team.ts
@@ -0,0 +1,30 @@
+import { Model, INTEGER, STRING } from 'sequelize';
+import db from '.';
+
+class Team extends Model {
+  id: number;
+  teamName: string;
+}
+
+Team.init(
+  {
+    id: {
+      type: INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true,
+    },
+    teamName: {
+      type: STRING,
+      allowNull: false,
+    },
+  },
+  {
+    underscored: true,
+    sequelize: db,
+    modelName: 'teams',
+    timestamps: false,
+  },
+);
+
+export default Team;
