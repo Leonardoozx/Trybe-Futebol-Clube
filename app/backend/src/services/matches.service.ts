b9f8cf6577ae814f1f0d9f24c8d30ae6afc9f5bc
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-10-26 19:40:27 -0300

    feat: a match class method for updating a match in db

--- app/backend/src/services/matches.service.ts
+++ app/backend/src/services/matches.service.ts
@@ -1,3 +1,4 @@
+import IMatchUpdate from '../interfaces/IMatchUpdate';
 import Team from '../database/models/Team';
 import Match from '../database/models/Match';
 
@@ -35,10 +36,16 @@ class TeamsServices {
       ],
     });
 
-  public insertMatch = (body: object) => Match.create({ ...body, inProgress: true });
+  public insertMatch = (body: object) =>
+    Match.create({ ...body, inProgress: true });
 
   public finishMatch = async (id: number): Promise<void> => {
     await Match.update({ inProgress: false }, { where: { id } });
+  };
+
+  public updateMatch = async (id: string, body: IMatchUpdate) => {
+    const { homeTeamGoals, awayTeamGoals } = body;
+    await Match.update({ homeTeamGoals, awayTeamGoals }, { where: { id } });
   };
 }
 
