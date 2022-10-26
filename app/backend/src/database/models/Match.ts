6ec2b125b1b37bf924a6a100595d270cb95c3d6d
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-10-26 17:35:08 -0300

    fix: now the relationships between Match and Team model, is working correctly

--- app/backend/src/database/models/Match.ts
+++ app/backend/src/database/models/Match.ts
@@ -48,10 +48,10 @@ Match.init(
   },
 );
 
-Match.belongsToMany(Team, { foreignKey: 'homeTeam', through: Match, as: 'home_team' });
-Match.belongsToMany(Team, { foreignKey: 'awayTeam', through: Match, as: 'away_team' });
+Match.belongsTo(Team, { foreignKey: 'homeTeam', as: 'teamHome' });
+Team.hasMany(Match, { foreignKey: 'id', as: 'teamHome' });
 
-Team.hasMany(Match, { foreignKey: 'homeTeam', as: 'home_team' });
-Team.hasMany(Match, { foreignKey: 'awayTeam', as: 'away_team' });
+Match.belongsTo(Team, { foreignKey: 'awayTeam', as: 'teamAway' });
+Team.hasMany(Match, { foreignKey: 'id', as: 'teamAway' });
 
 export default Match;
