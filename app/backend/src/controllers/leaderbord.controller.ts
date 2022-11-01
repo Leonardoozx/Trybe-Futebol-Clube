4b332f556e76bc952cbaed1e187e11965ece6cc4
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-10-31 21:51:22 -0300

    refactor: now the getHomeTeams controller is cleaner

--- app/backend/src/controllers/leaderbord.controller.ts
+++ app/backend/src/controllers/leaderbord.controller.ts
@@ -1,4 +1,5 @@
 import * as express from 'express';
+import sortLeaderboardTeams from '../utils/sortLeaderboardTeams';
 import LeaderbordServices from '../services/leaderbord.service';
 
 class LeaderbordController {
@@ -6,7 +7,7 @@ class LeaderbordController {
 
   public getHomeTeams: express.RequestHandler = async (_req, res) => {
     const allHomeTeams = await this._leaderbordServices.getHomeTeams();
-    res.status(200).json(allHomeTeams);
+    res.status(200).json(sortLeaderboardTeams(allHomeTeams));
   };
 }
 
