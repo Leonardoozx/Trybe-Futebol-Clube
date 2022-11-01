50e30e776321189eaf5e5d2a42ba7d6e66c242a1
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-10-31 22:08:27 -0300

    feat: a service for getting away teams from DB

--- app/backend/src/services/leaderbord.service.ts
+++ app/backend/src/services/leaderbord.service.ts
@@ -90,6 +90,49 @@ class LeaderbordServices {
         })),
     );
   };
+
+  public getAwayTeams = async () => {
+    const allMatches = await this._matchServices.getAllEndedMatches();
+    const allTeamsInfos = await this._allHomeTeams();
+    await Promise.all(
+      allMatches.map(async (team) => {
+        const teamById = await this._teamsServices.getTeamById(
+          String(team.awayTeam),
+        );
+        const matchPoints = this._getMatchPoints(
+          team.awayTeamGoals,
+          team.homeTeamGoals,
+        );
+        for (let i = 0; i < allTeamsInfos.length; i += 1) {
+          if (allTeamsInfos[i].name === teamById?.teamName) {
+            allTeamsInfos[i].totalPoints += this._getMatchPoints(
+              team.awayTeamGoals,
+              team.homeTeamGoals,
+            );
+            allTeamsInfos[i].totalGames += 1;
+            allTeamsInfos[i].goalsFavor += team.awayTeamGoals;
+            allTeamsInfos[i].goalsOwn += team.homeTeamGoals;
+            allTeamsInfos[i].totalVictories += matchPoints === 3 ? 1 : 0;
+            allTeamsInfos[i].totalDraws
+              += team.homeTeamGoals === team.awayTeamGoals ? 1 : 0;
+            allTeamsInfos[i].totalLosses += matchPoints === 0 ? 1 : 0;
+            return;
+          }
+        }
+      }),
+
+    );
+    return Promise.all(
+      allTeamsInfos
+        .map((team) => ({
+          ...team,
+          goalsBalance: (team.goalsFavor - team.goalsOwn),
+          efficiency: String(
+            ((team.totalPoints / (team.totalGames * 3)) * 100).toFixed(2),
+          ),
+        })),
+    );
+  };
 }
 
 export default LeaderbordServices;
