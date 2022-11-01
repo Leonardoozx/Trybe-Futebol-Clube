4aeae98fe8779a2a24fed5bbdd7f242c8da04012
Leonardoozx <leonardo2025maldarck@gmail.com>
2022-10-31 22:08:03 -0300

    fix: now the loeaderboard service name is correctly
    feat: a controller for getting all away teams from DB

--- app/backend/src/controllers/leaderbord.controller.ts
+++ app/backend/src/controllers/leaderbord.controller.ts
@@ -3,11 +3,16 @@ import sortLeaderboardTeams from '../utils/sortLeaderboardTeams';
 import LeaderbordServices from '../services/leaderbord.service';
 
 class LeaderbordController {
-  constructor(private _leaderbordServices = new LeaderbordServices()) {}
+  constructor(private _leaderboardServices = new LeaderbordServices()) {}
 
   public getHomeTeams: express.RequestHandler = async (_req, res) => {
-    const allHomeTeams = await this._leaderbordServices.getHomeTeams();
+    const allHomeTeams = await this._leaderboardServices.getHomeTeams();
     res.status(200).json(sortLeaderboardTeams(allHomeTeams));
+  };
+
+  public getAwayTeams: express.RequestHandler = async (_req, res) => {
+    const allAwayTeams = await this._leaderboardServices.getAwayTeams();
+    res.status(200).json(sortLeaderboardTeams(allAwayTeams));
   };
 }
 
